dc02f7bc08fc5e63936f3029aaa0614acd62dc0e
Josh Hanson <joshhanson@Joshs-MacBook-Pro.local>
2017-10-13 21:30:01 -0400

    Add lcbo api

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -10,7 +10,7 @@ export class ApiProvider {
         return this.http.get('https://swapi.co/api/films').map(res => res.json());
     }
 
-    // getBeers() {
-    //     return this.http.get('https://lcboapi.com/products/460881?access_key=MDpkODNhZTFhNi0xNGY4LTExZTctODAyYS05YmU5YWE3YmEzNTU6SzRQSVo3d0ZWMGhheGNjTGVjM3AwelpoNHNsSG5mU0FEaG9k').map(res => res.json());
-    // }
+    getBeers() {
+        return this.http.get('https://lcboapi.com/products/460881?access_key=MDpiYTFjODVhNi1iMDY4LTExZTctOGNhMS0yYjM2ZTNlMDFjY2E6c2J4eVVBVWdFbk1zNUhvUG9ralQ3ZmQ3N2Q1N0FOYzBZY0RL').map(res => res.json());
+    }
 }
