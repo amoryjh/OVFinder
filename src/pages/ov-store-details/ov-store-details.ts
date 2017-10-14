50128d2bec0c818ee18b585a9a5c74ba1ad2305b
Josh Hanson <joshhanson@Joshs-MacBook-Pro.local>
2017-10-14 18:51:52 -0400

    New build

--- src/pages/ov-store-details/ov-store-details.ts
+++ src/pages/ov-store-details/ov-store-details.ts
@@ -23,8 +23,4 @@ export class OvStoreDetailsPage {
 
         return (addressTwo == null ? addressOne : addressOne + ", " + addressTwo);
     }
-
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad OvStoreDetailsPage');
-    }
 }
