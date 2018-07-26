6da88576bcf73839f15508a53fb796bf06d3d770
Josh Hanson <joshhazn@gmail.com>
2018-07-26 12:56:56 -0400

    Convert project from ionic to react

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { GoogleApiWrapper } from 'google-maps-react'
+import './App.css';
+import MapContainer from './MapContainer'
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            stores: [],
+            locations: [],
+            prodID: '460881',
+        }
+
+        this.updateList = this.updateList.bind(this);
+    }
+
+    getStores(prodID) {
+        fetch('https://lcboapi.com/stores?access_key=MDpiYTFjODVhNi1iMDY4LTExZTctOGNhMS0yYjM2ZTNlMDFjY2E6c2J4eVVBVWdFbk1zNUhvUG9ralQ3ZmQ3N2Q1N0FOYzBZY0RL&lat=43.65838&lon=-79.44335&product_id=' + prodID)
+            .then(results => {
+                return results.json();
+            }).then(data => {
+                let stores = data.result.map((store) => {
+                    return (
+                        <li>
+                            <h1>{store.name}</h1>
+                            <h2>{store.city}, <small>{store.address_line_1} {store.address_line_2} </small> </h2>
+                            <p className="cases">Cases Left: {store.quantity}</p>
+                        </li>
+                    )
+                })
+                this.setState({ stores: stores })
+                this.setState({ locations: data.result })
+            })
+    }
+
+    componentDidMount() {
+        this.getStores(this.state.prodID);
+    }
+
+    updateList(event) {
+        var newProdID = event.target.value;
+        this.setState({ prodID: newProdID, stores: [] });
+        this.getStores(newProdID);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">OV Finder - React</h1>
+                </header>
+
+                <MapContainer locations={this.state.locations} google={this.props.google} />
+
+
+                <ul className="store-list">
+                    <br />
+                    <br /><br />
+                    <h2>Select Your Case:</h2>
+                    <select onChange={this.updateList} value={this.state.prodID}>
+                        <option id="460881" value="460881">6 Case - Tall Cans</option>
+                        <option id="900365" value="900365">12 Case - Bottles</option>
+                        <option id="360446" value="360446">15 Case - Cans</option>
+                        <option id="902056" value="902056">24 Case - Bottles</option>
+
+                    </select>
+                    {this.state.stores}
+                </ul>
+            </div>
+        );
+    }
+}
+
+// export default App;
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyBE2vcZEr-EcYBbDwfklzYrf8L0cPi9U2k',
+})(App)
+
+
---
+++ src/MapContainer.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+
+
+export default class MapContainer extends Component {
+
+    componentDidUpdate() {
+        this.loadMap(); // call loadMap function to load the google map
+    }
+
+    loadMap() {
+        if (this.props && this.props.google) { // checks to make sure that props have been passed
+            const { google } = this.props; // sets props equal to google
+            const maps = google.maps; // sets maps to google maps props
+
+            const mapRef = this.refs.map; // looks for HTML div ref 'map'. Returned in render below.
+            const node = ReactDOM.findDOMNode(mapRef); // finds the 'map' div in the React DOM, names it node
+
+            const mapConfig = Object.assign({}, {
+                center: { lat: 43.65838, lng: -79.44335 }, 
+                zoom: 8,
+                mapTypeId: 'roadmap'
+            })
+
+            this.map = new maps.Map(node, mapConfig); // creates a new Google map on the specified node (ref='map') with the specified configuration set above.
+
+            this.props.locations.forEach(location => { // iterate through locations saved in state
+                console.log(location);
+
+                let contentString = "<h2>" + location.name + " LCBO, " 
+                    + location.city + 
+                    "</h2> <p>Cases Left: <strong> " + location.quantity + 
+                    " </strong ></p> <p>Distance: " + (location.distance_in_meters / 1000)  + "KM</p>";
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                const marker = new google.maps.Marker({
+                    position: { lat: location.latitude, lng: location.longitude }, 
+                    map: this.map,
+                    title: location.name 
+                });
+
+                marker.addListener('click', function () {
+                    infowindow.open(this.map, marker);
+                });
+
+                this.map.addListener('click', function () {
+                    infowindow.close();
+                });
+            })
+        }
+    }
+
+    render() {
+        const style = { 
+            width: '100vw', 
+            height: '40vh',
+            minHeight: '450px'
+        }
+
+        return (
+            <div ref="map" style={style}>
+                loading map...
+            </div>
+        )
+    }
+}
